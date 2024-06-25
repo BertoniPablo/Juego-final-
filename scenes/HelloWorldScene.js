1823c8895b45791e2db0ec3ce6c674a528de6510
BertoniPablo <bertonipablo3@gmail.com>
2024-06-25 13:32:58 -0300

    lluvia de oreos

--- scenes/HelloWorldScene.js
+++ scenes/HelloWorldScene.js
@@ -8,39 +8,104 @@ export default class HelloWorldScene extends Phaser.Scene {
   }
 
   init() {
-    // this is called before the scene is created
-    // init variables
-    // take data passed from other scenes
-    // data object param {}
+    this.gameOver = false;
+    this.timer = 30;
+    this.score = 0;
+    this.shapes = {
+      oreos: { points: 10, count: 0 },
+      cuadrado: { points: 20, count: 0 },
+      rombo: { points: 30, count: 0 },
+      bomb: { points: -10, count: 0 },
+    };
   }
 
   preload() {
-    // load assets
-    this.load.image("sky", "./assets/space3.png");
-    this.load.image("logo", "./assets/phaser3-logo.png");
-    this.load.image("red", "./assets/particles/red.png");
+    
+    //import Fondo
+    this.load.image("Fondo", "./public/assets/Fondo.jpeg");
+  
+    //import plataforma
+   this.load.image("arena", "./public/assets/arena.png");
+    
+    //import personaje
+    this.load.image("personaje", "./public/assets/pez1.png");
+
+    // importar recolectable
+    this.load.image("oreos", "./public/assets/oreo.png");
   }
 
   create() {
-    // create game objects
-    this.add.image(400, 300, "sky");
+    this.Fondo = this.add.image(400, 300, "Fondo");
+    this.Fondo.setScale(2);
 
-    const logo = this.physics.add.image(400, 100, "logo");
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+    // crear grupo Plataformas
+    this.arena = this.physics.add.staticGroup();
+    // al grupo de Plataformas agregar una plataforma
+    this.arena.create(400, 568, "arena").setScale(2).refreshBody();
 
-    // emmit particles from logo
-    const emitter = this.add.particles(0, 0, "red", {
-      speed: 100,
-      scale: { start: 1, end: 0 },
-      blendMode: "ADD",
+    //crear personaje
+    this.personaje = this.physics.add.sprite(400, 300, "personaje");
+    this.personaje.setScale(0.2);
+    this.personaje.setCollideWorldBounds(true);
+
+    //agregar colision entre personaje y plataforma
+    this.physics.add.collider(this.personaje, this.arena);
+
+    //crear teclas
+    this.cursor = this.input.keyboard.createCursorKeys();
+
+   //agregar texto de timer en la esquina superior derecha
+    this.timerText = this.add.text(10, 10, `tiempo restante: ${this.timer}`, {
+      ontSize: "32px",
+      fill: "#000000",
+       });
+    
+     // evento 1 segundo
+     this.time.addEvent({
+      delay: 1000,
+      callback: this.onSecond,
+      callbackScope: this,
+      loop: true,
     });
 
-    emitter.startFollow(logo);
+    this.scoreText = this.add.text(
+      10,
+      50,
+      `Puntaje: ${this.score}
+        T: ${this.shapes["oreos"].count}
+      `);
+      // crear grupo recolectables
+      this.recolectables = this.physics.add.group();
+
   }
 
   update() {
-    // update game objects
+
+      // movimiento personaje
+      if (this.cursor.left.isDown) {
+        this.personaje.setVelocityX(-160);
+      } else if (this.cursor.right.isDown) {
+        this.personaje.setVelocityX(160);
+      } else {
+        this.personaje.setVelocityX(0);
+      }
+      if (this.cursor.up.isDown && this.personaje.body.touching.down) {
+        this.personaje.setVelocityY(-330);
+      }
+  }
+  
+  onSecond() {
+    if (this.gameOver) {
+      return;
+    }
+    // crear recolectable
+    const tipos = ["oreos"];
+
+    const tipo = Phaser.Math.RND.pick(tipos);
+    let recolectable = this.recolectables.create(
+      Phaser.Math.Between(10, 790),
+      0,
+      tipo
+    );
   }
 }
